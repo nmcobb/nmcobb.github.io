04dd55181e22ebc798bf2f60e6a735f3e112866f
nmcobb <miles.cobb@gmail.com>
2020-09-12 21:48:08 -0700

    first build

---
+++ precache-manifest.03317db679c6ade8f63cc25c9ef2cebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86321a29dd82737772bade821822e03a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54218edf1a23a8b4915e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94c2208cebe808603c5e",
+    "url": "/static/js/2.ec9a86a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec9a86a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54218edf1a23a8b4915e",
+    "url": "/static/js/main.92a43316.chunk.js"
+  },
+  {
+    "revision": "4efdb4cf7c627f162423",
+    "url": "/static/js/runtime-main.5bd0563f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
